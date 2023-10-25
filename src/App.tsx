62608308c8eaaa56019aeb220724f2d88225cbd1
Anchovia <hodolye@naver.com>
2023-10-25 17:28:37 +0900

    1. 글로벌 스타일 지정., 2. 기본 시작 라우팅(레이아웃과 홈 지정), 3. 네비게이션바 제작 시작 및 로고 텍스트 네비게이션 과 텍스트 네비게이션 기본 형태 틀 제작 완료.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/Layout"
+import Home from "./routes/Home"
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "",
+        element: <Home/>,
+      }
+    ]
+  },
+])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    background-color: white;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+  a{
+    text-decoration: none;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+`;
 
 function App() {
   return (
-    <></>
+    <Wrapper>
+      <GlobalStyles/>
+      <RouterProvider router={router}/>
+    </Wrapper>
   )
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,72 @@
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-rows: 100px 1fr;
+`;
+
+const Menu = styled.div`
+    width: 100%;
+    height:100%;
+    display: grid;
+    grid-template-columns: 1fr 2.571428fr 1fr;
+`;
+
+const MenuItem = styled.div`
+    width: 100%;
+    height:100%;
+    display: grid;
+    place-items: center;
+`;
+
+const LogoTxt = styled.div`
+    font-size: 40px;
+    letter-spacing: 0.025em;
+    font-weight: 900;
+    color: black;
+`;
+
+const TxtNav = styled.div`
+    width: 100%;
+    height:100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    place-items: center;
+`;
+
+const TxtNavTxt = styled.div`
+    font-size: 20px;
+    letter-spacing: -0.1em;
+    color: black;
+`;
+
+export default function Layout(){
+    const menuItems = [
+        { name: "햄스토리", path: "/hamstory" },
+        { name: '커뮤니티', path: '/community' },
+        { name: '위키', path: '/wiki' },
+        { name: '도움말', path: '/help' },
+    ];
+    
+    return (
+        <Wrapper>
+            <Menu>
+                <Link to="/">
+                    <MenuItem>
+                        <LogoTxt>HAMSTORY</LogoTxt>
+                    </MenuItem>
+                </Link>
+                <TxtNav>
+                    {menuItems.map((item, index) => (
+                        <Link key={index} to={item.path}><MenuItem><TxtNavTxt>{item.name}</TxtNavTxt></MenuItem></Link>
+                    ))}
+                </TxtNav>
+                <MenuItem></MenuItem>
+            </Menu>
+            <Outlet/>
+        </Wrapper>
+    );
+}
