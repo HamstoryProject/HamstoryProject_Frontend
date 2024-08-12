35fbc9ab5c2921343844fff1f7b8e55ff83c8e98
Anchovia <hodolye@naver.com>
2024-08-12 17:30:36 +0900

    추가 수정

--- src/components/community/Info.tsx
+++ src/components/community/Info.tsx
@@ -30,7 +30,7 @@ export default function Info() {
             <Wrapper>
                 <CommunityHeader/>
                 <Main>
-                    <Contents/>
+                    <></>
                 </Main>
             </Wrapper>
         </Body>
